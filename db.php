41c8fcd761ab10cdc539b059ac9b3fba3d45eb20
ikiK <kiki.ikik@gmail.com>
2021-06-12 15:45:06 +0200

    Delete db.php

--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost", "appdizinformatik_sniffer", "M7bNwsIMRJ8K", "appdizinformatik_sniffer");
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
