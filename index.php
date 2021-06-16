3d140f6c18bb29e9f74662c9f79bdb151d4113a7
ikiK <kiki.ikik@gmail.com>
2021-06-17 01:39:01 +0200

    -a

--- index.php
+++ index.php
@@ -11,9 +11,12 @@ $array = wait(parallelMap([
     'https://germany.zapwp.net/key:51f0429e31298bf7461bed0a35589cb68d1babce/q:intelligent/retina:false/webp:true/w:1/url:https:/vloryas.com/wp-content/uploads/2019/11/h1-testimonials-img-2-1-768x522.jpg',
     'https://wpcdn.us-east-1.vip.tn-cloud.net/www.channel3000.com/content/uploads/2020/03/Baby-Yoda.jpg',
     'https://newyork.zapwp.net/key:51f0429e31298bf7461bed0a35589cb68d1babce/q:intelligent/retina:false/webp:true/w:1/url:https:/vloryas.com/wp-content/uploads/2019/11/h1-testimonials-img-2-1-768x522.jpg',
+    'https://dl1.cbsistatic.com/i/2020/05/04/18018b2a-91b1-40b5-b91a-4412a10fac7f/aedc25fa28604a2a87d3b7df56cb4877/imgingest-9025120176881342416.png',
 ], function ($url) {
+    $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
     $ch = curl_init();
 
+    curl_setopt($curl, CURLOPT_USERAGENT, $agent);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -39,7 +42,7 @@ foreach ($array as $val) {
     echo 'Response Time: ' . $val[5] . '<br>';
     echo 'Response Type: ' . $val[2] . '<br><br>';
 
-    $base =  imageToBase64($val[4]);
+    $base =  imageToBase64($val[4], $val[2]);
 
     if ($val[0] != 200) {
         $msg = "Error \n code 0";
@@ -58,8 +61,8 @@ foreach ($array as $val) {
     }
 }
 
-function imageToBase64($image)
+function imageToBase64($image, $type)
 {
     $imageData = base64_encode($image);
-    return 'data: image/jpg;base64,' . $imageData;
+    return 'data: ' . $type . ';base64,' . $imageData;
 }
